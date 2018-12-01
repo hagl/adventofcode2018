c329650211953f58b7f9a14324f648141aebf546
Harald Gliebe <harald.gliebe@gmail.com>
2018-12-01 20:47:04 +0100

    Day 1, part 2

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,13 +1,26 @@
 import System.Environment (getArgs)
 
-calculateFrequency :: String -> Int
-calculateFrequency = sum . (map parseInt) . lines where
-  parseInt ('+':ls) = read(ls) :: Int
-  parseInt (s) = read(s) :: Int
+parseInt :: String -> Int
+parseInt ('+':ls) = read(ls) :: Int
+parseInt (s) = read(s) :: Int
 
+calculateFrequency :: String -> Int
+calculateFrequency = sum . (map parseInt) . lines 
+
+findDuplicateFrequencyFromText :: String -> Int
+findDuplicateFrequencyFromText = findDuplicateFrequency . (map parseInt) . lines
+
+findDuplicateFrequency :: [Int] -> Int
+findDuplicateFrequency input = loop 0 [0] repeatedInput where
+  repeatedInput = input ++ repeatedInput
+  loop f cache (x:xs) = let new = f + x in if (new `elem` cache) 
+    then new
+    else loop new (new:cache) xs
+    
 processFile inputFile = do
   input <- readFile inputFile
   putStrLn (show (calculateFrequency input))
+  putStrLn (show (findDuplicateFrequencyFromText input))
 
 main = mainWith myFunction
   where 
