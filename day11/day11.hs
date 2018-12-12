3c3a8db4f9cbf38f95113d4ca1230bd5212cc921
Harald Gliebe <harald.gliebe@gmail.com>
2018-12-12 09:03:55 +0100

    Day 11 part 2 fix

--- day11/day11.hs
+++ day11/day11.hs
@@ -18,7 +18,7 @@ rect (x0, y0, size) = sum [values ! (x,y) | x <- [x0..(x0+size-1)], y <- [y0..(y
 vals = [(x,y) | x<-[1..300], y <-[1..300]]
 
 maxSize (x, y) = let maxSize = min (300 - x) (300 - y) in
-  let incr s previous = previous + sum [(values ! (x + s, y + d)) + (values ! (x + d, y + s))| d <- [0 .. (s-1)]] + values ! (x + s, x + s) in
+  let incr s previous = previous + sum [(values ! (x + s, y + d)) + (values ! (x + d, y + s))| d <- [0 .. (s-1)]] + values ! (x + s, y + s) in
   let step (m@(k, v), p) s = let n = (incr s p) in if (n > v) then ((s, n), n) else (m, n) in
   let ((i,v),_) = foldl' step ((-1, -1000), 0) [0..maxSize] in 
     ((x,y,i+1), v)
